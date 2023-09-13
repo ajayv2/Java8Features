6563d5c61d10584c9f0cdaf30e7006a506a3015e
Ajay Upadhyay <ajay.upadhyay@v2solutions.com>
2023-09-13 15:45:33 +0530

    commit

--- src/com/java8/streamapi/Program10.java
+++ src/com/java8/streamapi/Program10.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 //program to find duplicate with its count
 public class Program10 {
     public static void main(String[] args) {
-        String[] str = {"AA","BB","AA","CC"};
+        String[] str = {"A","B","A","C"};
         List<String> list = Arrays.asList(str);
         Map<String, Long> result = Arrays.stream(str).filter(x -> Collections.frequency(list, x) > 1)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
